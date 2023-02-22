7bf10685170c18ee66b4da54750406dd33174619
Joseph Bunao <jbunao@aim.edu>
2023-02-22 09:34:01 -0500

    save as h5

--- core/model_training.py
+++ core/model_training.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def load_images(directory_path="data/Data for test", image_size=(256, 256)):
+def load_images(directory_path="data/Data for test", image_size=(128, 128)):
     train_data, val_data = tf.keras.utils.image_dataset_from_directory(
         directory_path,
         validation_split=0.2,
@@ -18,7 +18,7 @@ def create_data_augmentation_pipeline():
     data_augmentation = tf.keras.Sequential([
         tf.keras.layers.RandomFlip("horizontal_and_vertical"),
         tf.keras.layers.RandomRotation(0.2),
-        # tf.keras.layers.RandomZoom(0.1)
+        tf.keras.layers.RandomZoom(0.1)
     ])
     return data_augmentation
 
@@ -26,15 +26,15 @@ def create_data_augmentation_pipeline():
 def create_image_classifier(image_size):
     classifier = tf.keras.Sequential([
         tf.keras.layers.Rescaling(1. / 255, input_shape=image_size + (3,)),
-        tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+        tf.keras.layers.Conv2D(16, 3, padding='same', activation='swish'),
         tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+        tf.keras.layers.Conv2D(32, 3, padding='same', activation='swish'),
         tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+        tf.keras.layers.Conv2D(64, 3, padding='same', activation='swish'),
         tf.keras.layers.MaxPooling2D(),
-        # tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128, activation='swish'),
         tf.keras.layers.Dense(3)
     ])
     return classifier
@@ -42,9 +42,9 @@ def create_image_classifier(image_size):
 
 def train_image_classifier(
         data_directory="data/Data for test",
-        save_to_directory="trained_model",
-        image_size=(256, 256),
-        n_epochs=5
+        save_to_directory="trained_model/",
+        image_size=(128, 128),
+        n_epochs=20
     ):
 
     train_data, val_data = load_images(
@@ -69,6 +69,6 @@ def train_image_classifier(
         epochs=n_epochs
     )
 
-    image_classifier.save(save_to_directory)
+    image_classifier.save(save_to_directory + "image_classifier.h5")
 
     return image_classifier, history
